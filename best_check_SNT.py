5ce44da6206efe61ffa484dcde80d1470f280b98
nbn-03 <98254107+nbn-03@users.noreply.github.com>
2024-02-03 09:58:55 +0700

    Add files via upload

---
+++ best_check_SNT.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+# Kiểm tra
+print(is_prime(17))  # Kết quả: True
+print(is_prime(25))  # Kết quả: False
